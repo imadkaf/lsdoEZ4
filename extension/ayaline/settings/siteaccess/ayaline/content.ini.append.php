b915a6f43b137e0cecb9a0bd7f2cccec3032e602
nlescure <nlescure@d7d630f7-2859-4ba2-aaf3-32abb50a90ba>
2011-04-22 12:37:51 +0000

    Commit initial d'eZ

---
+++ extension/ayaline/settings/siteaccess/ayaline/content.ini.append.php
@@ -0,0 +1,8 @@
+<?php /* #?ini charset="utf-8"?
+
+[VersionView]
+AvailableSiteDesignList[]
+AvailableSiteDesignList[]=ayaline
+AvailableSiteDesignList[]=admin2
+AvailableSiteDesignList[]=admin
+*/ ?>
---
+++ settings/override/site.ini.append.php
@@ -0,0 +1,13 @@
+<?php /* #?ini charset="utf-8"?
+
+[ExtensionSettings]
+ActiveExtensions[]
+ActiveExtensions[]=ezmultiupload
+ActiveExtensions[]=ezjscore
+ActiveExtensions[]=ezie
+ActiveExtensions[]=ezoe
+ActiveExtensions[]=ezodf
+ActiveExtensions[]=ezprestapiprovider
+ActiveExtensions[]=ayaline
+
+*/ ?>
