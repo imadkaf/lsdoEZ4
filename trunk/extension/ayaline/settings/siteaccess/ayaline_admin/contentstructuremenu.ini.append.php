0d3e392f9f79d46259ce29f4a95fdc7790ec326c
ajarry <ajarry@d7d630f7-2859-4ba2-aaf3-32abb50a90ba>
2011-05-19 07:51:07 +0000

    ajout pour la médiathèque

--- trunk/extension/ayaline/settings/siteaccess/ayaline_admin/contentstructuremenu.ini.append.php
+++ trunk/extension/ayaline/settings/siteaccess/ayaline_admin/contentstructuremenu.ini.append.php
@@ -15,5 +15,11 @@ ShowClasses[]=webcams
 ShowClasses[]=webcam
 ShowClasses[]=faqs
 ShowClasses[]=faq
+ShowClasses[]=ayaline_gallery
+ShowClasses[]=gallery_category_photo
+ShowClasses[]=gallery_gallery_photo
+ShowClasses[]=gallery_photo
+ShowClasses[]=gallery_category_video
+ShowClasses[]=gallery_video
 
 */ ?>
