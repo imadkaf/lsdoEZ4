b397ed0f2b881602efe72e694c677e5ae6c4141d
ajarry <ajarry@d7d630f7-2859-4ba2-aaf3-32abb50a90ba>
2011-05-20 14:29:40 +0000

    modif, plus propre (ceux qui ont été enlevé, c'est parcequ'ils été déjà déclaré dans les extensions)

--- trunk/extension/ayaline/settings/siteaccess/ayaline_admin/contentstructuremenu.ini.append.php
+++ trunk/extension/ayaline/settings/siteaccess/ayaline_admin/contentstructuremenu.ini.append.php
@@ -13,13 +13,7 @@ ShowClasses[]=rubric
 ShowClasses[]=accueil
 ShowClasses[]=webcams
 ShowClasses[]=webcam
-ShowClasses[]=faqs
-ShowClasses[]=faq
-ShowClasses[]=ayaline_gallery
-ShowClasses[]=gallery_category_photo
-ShowClasses[]=gallery_gallery_photo
 ShowClasses[]=gallery_photo
-ShowClasses[]=gallery_category_video
 ShowClasses[]=gallery_video
 
 */ ?>
