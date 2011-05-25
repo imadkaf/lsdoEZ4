f609da7afba94ddf70967a751cb078b95f110d32
ajarry <ajarry@d7d630f7-2859-4ba2-aaf3-32abb50a90ba>
2011-05-25 10:26:44 +0000

    maj des déclarations de js

--- extension/ayaline/settings/design.ini.append.php
+++ extension/ayaline/settings/design.ini.append.php
@@ -8,5 +8,11 @@ DesignExtensions[]=ayaline
 CSSFileList[]=main.css
 
 [JavaScriptSettings]
+# deja inclus dans l'extension ayaline_gallery
+#JavaScriptList[]=jquery.js
 JavaScriptList[]=script.js
+JavaScriptList[]=menu_gauche_interne.js
+JavaScriptList[]=main.js
+JavaScriptList[]=jquery.popin.pack.js
+
 */ ?>
