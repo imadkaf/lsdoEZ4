ff2921cbd405ddf307d338b2a260138a13278b7b
ajarry <ajarry@d7d630f7-2859-4ba2-aaf3-32abb50a90ba>
2011-06-09 15:42:03 +0000

    ajout pour exclure des classes de la recherche

---
+++ trunk/extension/ayaline/settings/siteaccess/ayaline/ezfind.ini.append.php
@@ -0,0 +1,16 @@
+<?php /* #?ini charset="utf-8"?
+
+[IndexExclude]
+ClassIdentifierList[]=folder
+ClassIdentifierList[]=embed_code
+ClassIdentifierList[]=ba_flux_rss_blog
+ClassIdentifierList[]=main_menu
+ClassIdentifierList[]=footer_menu
+ClassIdentifierList[]=sub_menu
+ClassIdentifierList[]=season
+ClassIdentifierList[]=topic
+ClassIdentifierList[]=lien_externe
+ClassIdentifierList[]=image
+ClassIdentifierList[]=diaporama
+
+*/ ?>
