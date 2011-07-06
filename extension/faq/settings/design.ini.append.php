256a1b3090aea056d7d34fb9f80bddcc3e60b4ac
ajarry <ajarry@d7d630f7-2859-4ba2-aaf3-32abb50a90ba>
2011-07-06 13:15:58 +0000

    - optimisation appels des .js

--- extension/faq/settings/design.ini.append.php
+++ extension/faq/settings/design.ini.append.php
@@ -8,8 +8,7 @@ DesignExtensions[]=faq
 CSSFileList[]=faq.css
 
 [JavaScriptSettings]
-# n'est pas utilise dans notre cas car jquery deja inclus dans une autre extension
-#JavaScriptList[]=jquery-1.5.1.min.js
+#JavaScriptList[]=jquery.js
 JavaScriptList[]=faq.js
 
 */ ?>
