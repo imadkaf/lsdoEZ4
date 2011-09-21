1aa9075fa5327d4fbfdfbc8975038b6be43c448a
yimzourh <yimzourh@d7d630f7-2859-4ba2-aaf3-32abb50a90ba>
2011-09-21 17:42:33 +0000

    améliorations SIT Liste

---
+++ extension/ez_aya_sit/cronjobs/alimentation_nouveaux_attributs_sit_liste.php
@@ -0,0 +1,51 @@
+<?php
+
+$cli->setUseStyles(true);
+
+if (!$isQuiet) {
+	$cli->output($cli->stylize('cyan', "\tDébut alimentation nouveaux attributs SIT liste."));
+}
+
+$fetchParameters = array(
+	'parent_node_id' => 2,
+	'depth' => 10,
+	'class_filter_type' => 'include',
+	'class_filter_array' => array('affichage_liste_sit')
+);
+
+$nodeObjects = eZFunctionHandler::execute(
+	'content',
+	'list',
+	$fetchParameters
+);
+
+foreach ($nodeObjects as $nodeObject) {
+	$nodeObjectDataMap = $nodeObject->dataMap();
+	$sitListeReliee = $nodeObjectDataMap['liaison_liste']->value();
+	
+	$sitListeRelieeDataMap = $sitListeReliee->dataMap();
+	
+	$originalThumbNail = $nodeObjectDataMap['thumbnail']->value()->attribute('original');
+
+	$attributeList = array(
+		'titre_liste' => $nodeObjectDataMap['title']->value(),
+		'thumbnail' => $originalThumbNail['url'],
+		'short_description' => $nodeObjectDataMap['short_description']->value(),
+		'googlemaps' => $nodeObjectDataMap['googlemaps']->value(),
+		'topics' => $nodeObjectDataMap['topics']->toString()
+	);
+
+	$res = eZContentFunctions::updateAndPublishObject($sitListeReliee, array('attributes' => $attributeList));
+	
+	if ($res) {
+		$cli->output($cli->stylize('green', "\tOK : ".$nodeObject->object()->ID."."));
+	} else {
+		$cli->output($cli->stylize('red', "\tKO : ".$nodeObject->object()->ID."."));
+	}
+}
+
+if (!$isQuiet) {
+	$cli->output($cli->stylize('green', "\tFait."));
+}
+
+?>
--- extension/ez_aya_sit/settings/cronjob.ini.append.php
+++ extension/ez_aya_sit/settings/cronjob.ini.append.php
@@ -6,4 +6,10 @@ ExtensionDirectories[]=ez_aya_sit
 [CronjobPart-importation_categories_criteres_sit]
 Scripts[]=importation_categories_criteres_sit.php
 
+[CronjobPart-alimentation_nouveaux_attributs_sit_liste]
+Scripts[]=alimentation_nouveaux_attributs_sit_liste.php
+
+[CronjobPart-reinitialisation_criteres_tri_principaux]
+Scripts[]=reinitialisation_criteres_tri_principaux.php
+
 */ ?>
