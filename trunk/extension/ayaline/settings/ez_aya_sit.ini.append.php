c8a9e403f35e92361d82610326bc4a5d2bcf29b2
ajarry <ajarry@d7d630f7-2859-4ba2-aaf3-32abb50a90ba>
2011-09-27 13:49:13 +0000

    début traduction des termes en dur des fichiers xsl + débug des fichiers .ini + Changement de la police des titres h2 (rubriques, listes SIT, ...)

--- trunk/extension/ayaline/settings/ez_aya_sit.ini.append.php
+++ trunk/extension/ayaline/settings/ez_aya_sit.ini.append.php
@@ -1,13 +1,15 @@
 <?php /* #?ini charset="utf-8"?
 
 [GlobalSitParameters]
-ListeVilles[]
 ListeVilles[]=85194|Les Sables d'Olonne
 ListeVilles[]=85060|Château d'Olonne
 ListeVilles[]=85166|Olonne sur Mer
 ListeVilles[]=85112|L'Ile-d'Olonne
 ListeVilles[]=85214|Sainte Foy
 ListeVilles[]=85298|Vairé
+
+[SitTranslations]
+StaticSitTranslations[]=Itineraire
 
 [ImagesDesign]
 Chemin=/extension/ayaline/design/ayaline/images/
