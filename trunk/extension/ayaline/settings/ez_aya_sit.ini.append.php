af6b27dae934de281fa1997f1c05a2c0f1953bfc
ajarry <ajarry@d7d630f7-2859-4ba2-aaf3-32abb50a90ba>
2011-11-08 08:42:18 +0000

    ordre alphabétique des villes

--- trunk/extension/ayaline/settings/ez_aya_sit.ini.append.php
+++ trunk/extension/ayaline/settings/ez_aya_sit.ini.append.php
@@ -1,10 +1,10 @@
 <?php /* #?ini charset="utf-8"?
 
 [GlobalSitParameters]
-ListeVilles[]=85194|Les Sables d'Olonne
 ListeVilles[]=85060|Château d'Olonne
-ListeVilles[]=85166|Olonne sur Mer
+ListeVilles[]=85194|Les Sables d'Olonne
 ListeVilles[]=85112|L'Ile-d'Olonne
+ListeVilles[]=85166|Olonne sur Mer
 ListeVilles[]=85214|Sainte Foy
 ListeVilles[]=85298|Vairé
 
