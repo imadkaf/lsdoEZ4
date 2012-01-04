81865e4cf31b1fce0a03c5fc4e62d34eac5b2d9c
melbajjati <melbajjati@d7d630f7-2859-4ba2-aaf3-32abb50a90ba>
2012-01-04 18:22:10 +0000

    la commande pour purger solr : curl http://localhost:8983/solr/update?commit=true -H "Content-Type:text/xml" --data-binary '<delete><query>*:*</query></delete>'

--- extension/ez_aya_sitsearch/cronjobs/indexation_contenus_sit.php
+++ extension/ez_aya_sitsearch/cronjobs/indexation_contenus_sit.php
@@ -1,4 +1,4 @@
-<?php
+﻿<?php
 //require_once('extension/ez_aya_sitsearch/classes/sit_utils.class.php');
 
 if(!$isQuiet){
@@ -27,11 +27,11 @@ $nodeObjects = eZFunctionHandler::execute(
 $http = eZHTTPTool::instance();
 
 $sitIni = eZINI::instance('ez_aya_sitsearch.ini');
-
+$cpt = 0;
 foreach ($nodeObjects as $nodeObject) {
-	
+	if ($cpt == 10) break;
 	$lienCourant = $nodeObject->attribute('url_alias');
-	//echo $lienCourant;
+	
 	eZURI::transformURI($lienCourant);
 	
 	$sitListe = $nodeObject->dataMap();
@@ -48,8 +48,9 @@ foreach ($nodeObjects as $nodeObject) {
 	$dureeVieCache = $sitListe['duree_vie_cache']->value() * 60;
 	
 	$sitParams = array();
-	$sitParams['dwcom'] = "11111";
-	
+	$sitParams['dwcom'] = "10000";
+	$sitParams['dwdate'] = "111";
+		
 	$parametresDiffusionSupplementaires = $sitListe['parametres_diffusion_supplementaires']->value()->attribute('matrix');
 	$parametresDiffusionSupplementaires = $parametresDiffusionSupplementaires['rows']['sequential'];
 	
@@ -76,49 +77,132 @@ foreach ($nodeObjects as $nodeObject) {
 	$sitParamsString .= $categorie ? "&idc=".$categorie : "";
 	$sitParamsEncodedString .= $categorie ? "&idc=".$categorie : "";
 	$url_fiche_xml = $rootSitUrl."Recherche".$sitParamsEncodedString;
+	
 	//$contenuXmlDistant = SitUtils::urlGetContentsCurl($rootSitUrl."Recherche".$sitParamsEncodedString, 120);
 	$xml = new SimpleXMLElement($url_fiche_xml, null, true);
-	
-	$nodes = $xml->xpath('//resultats/details/detail');
 
-	foreach($nodes as $node){
+	$produits = $xml->xpath('//resultats/produit');	
+	$details = $xml->xpath('//resultats/details/detail');
+	
+	foreach($details as $detail){
+		if ($cpt == 10) break;
+		$cpt++;
+		//var_dump($detail);
+		$id_detail = (string) $detail->attributes()->id;
+		
+		foreach ($produits as $produit){
+			$id_produit = (string) $produit->attributes()->id;
+			
+			if(  $id_produit == $id_detail ){
+				$nb_consultations = $produit->attributes()->nbConsultations;
+				
+			}
+		}
+		
 		$note_elt = $doc->createElement('doc');
 		$root_elt->appendChild($note_elt);
 		
 		$carac = array(" "); 
-		$intitule = str_replace($carac,'-', $node->intitule);
+		$intitule = utf8_encode(str_replace($carac,'-', trim($detail->intitule)));
+		$intitule = preg_replace("/ & /si",' &amp; ', $intitule);
 		
 		$caracs = array("/");
 		$lien = substr($lienCourant, 1);
 		$alias = str_replace($caracs,'~', $lien);
 		
-		$url = 'Fiche/Detail/'.$node->attributes()->id.'/'.$alias.'/'.$intitule;
-		var_dump($url);
+		$url = 'Fiche/Detail/'.$detail->attributes()->id.'/'.$alias.'/'.$intitule;
+		//var_dump($url);
 		
-		$id_field = $doc->createElement('field', $node->attributes()->id);
-		$id_field->setAttribute('name', 'id');
-		$note_elt->appendChild($id_field);
+		/*$meta_installation_id_ms = $doc->createElement('field', eZSolr::installationID());
+		$meta_installation_id_ms->setAttribute('name', 'meta_installation_id_ms');
+		$note_elt->appendChild($meta_installation_id_ms);*/ 
 		
-		$intitule_field = $doc->createElement('field', $node->intitule);
-		$intitule_field->setAttribute('name', 'intitule');
-		$note_elt->appendChild($intitule_field);
+		/*$meta_main_node_id_si= $doc->createElement('field', '99999');
+		$meta_main_node_id_si->setAttribute('name', 'meta_main_node_id_si');
+		$note_elt->appendChild($meta_main_node_id_si);*/
 		
-		//$commentaire1 = preg_replace("/&([\\w\\d]+|\\#\\d+);/si",' ', $node->commentaires->commentaire1);
-		$cmt1_field = $doc->createElement('field', $node->commentaires->commentaire1);
-		$cmt1_field->setAttribute('name', 'commentaire1');
+		$meta_installation_url_ms= $doc->createElement('field', '/');
+		$meta_installation_url_ms->setAttribute('name', 'meta_installation_url_ms');
+		$note_elt->appendChild($meta_installation_url_ms);
+		
+		$meta_is_invisible_b = $doc->createElement('field', 'false');
+		$meta_is_invisible_b->setAttribute('name', 'meta_is_invisible_b');
+		$note_elt->appendChild($meta_is_invisible_b); 
+		
+		$meta_path_si = $doc->createElement('field', '99999');
+		$meta_path_si->setAttribute('name', 'meta_path_si');
+		$note_elt->appendChild($meta_path_si);
+		
+		$meta_path_string_ms = $doc->createElement('field', '/99999/');
+		$meta_path_string_ms->setAttribute('name', 'meta_path_string_ms');
+		$note_elt->appendChild($meta_path_string_ms); 
+		
+		$meta_guid_ms = $doc->createElement('field', md5(eZSolr::installationID().'-'."sit_fiche_".$detail->attributes()->id.'-'."fre-FR"));
+		$meta_guid_ms->setAttribute('name', 'meta_guid_ms');
+		$note_elt->appendChild($meta_guid_ms);
+		
+		$meta_section_id_si = $doc->createElement('field', "1");
+		$meta_section_id_si->setAttribute('name', 'meta_section_id_si');
+		$note_elt->appendChild($meta_section_id_si);				
+		
+		$intitule_field_s = $doc->createElement('field', $detail->intitule);
+		$intitule_field_s->setAttribute('name', 'attr_name_s');
+		$note_elt->appendChild($intitule_field_s);
+		
+		$intitule_field_t = $doc->createElement('field', $detail->intitule);
+		$intitule_field_t->setAttribute('name', 'attr_name_t');
+		$note_elt->appendChild($intitule_field_t);
+		
+		$commentaire1 = preg_replace("/&euro;/si",'€', utf8_encode((string) $detail->commentaires->commentaire1));
+		$commentaire1 = preg_replace("/\\\\n|\r/si",'', $commentaire1);
+		$commentaire1 = preg_replace("/ & /si",' &amp; ', $commentaire1);
+		$cmt1_field = $doc->createElement('field', trim($commentaire1));
+		$cmt1_field->setAttribute('name', 'attr_content_s');
 		$note_elt->appendChild($cmt1_field);
 		
-		$cmt2_field = $doc->createElement('field', $node->commentaires->commentaire2);
-		$cmt2_field->setAttribute('name', 'commentaire2');
-		$note_elt->appendChild($cmt2_field);
+		$available_language_field = $doc->createElement('field', 'fre-FR');
+		$available_language_field->setAttribute('name', 'meta_available_language_codes_ms');
+		$note_elt->appendChild($available_language_field);
 		
-		$url_field = $doc->createElement('field', $url);
-		$url_field->setAttribute('name', 'url');
-		$note_elt->appendChild($url_field);
+		$language_field = $doc->createElement('field', 'fre-FR');
+		$language_field->setAttribute('name', 'meta_language_code_ms');
+		$note_elt->appendChild($language_field);
+		
+		$main_url_alias_field = $doc->createElement('field', $url);
+		$main_url_alias_field->setAttribute('name', 'meta_main_url_alias_ms');
+		$note_elt->appendChild($main_url_alias_field);
+		
+		$meta_name_t_field = $doc->createElement('field', $detail->intitule);
+		$meta_name_t_field->setAttribute('name', 'meta_name_t');
+		$note_elt->appendChild($meta_name_t_field);
 		
 		
-		$doc->formatOutput = true;	
+		$date_maj = str_replace($carac,'T', $detail->dateMAJ).'Z';
+		$meta_modified_field = $doc->createElement('field',$date_maj);
+		$meta_modified_field->setAttribute('name', 'meta_modified_dt');
+		$note_elt->appendChild($meta_modified_field);
 		
+		$meta_published_field = $doc->createElement('field', $date_maj);
+		$meta_published_field->setAttribute('name', 'meta_published_dt');
+		$note_elt->appendChild($meta_published_field);
+		
+		$meta_sort_name_ms = $doc->createElement('field', $detail->intitule);
+		$meta_sort_name_ms->setAttribute('name', 'meta_sort_name_ms');
+		$note_elt->appendChild($meta_sort_name_ms);
+		
+		$meta_url_alias_ms = $doc->createElement('field', $url);
+		$meta_url_alias_ms->setAttribute('name', 'meta_url_alias_ms');
+		$note_elt->appendChild($meta_url_alias_ms);
+		
+		$meta_is_hidden_b = $doc->createElement('field', 'false');
+		$meta_is_hidden_b->setAttribute('name', 'meta_is_hidden_b');
+		$note_elt->appendChild($meta_is_hidden_b);
+		
+		$meta_view_count_si = $doc->createElement('field', $nb_consultations);
+		$meta_view_count_si->setAttribute('name', 'meta_view_count_si');
+		$note_elt->appendChild($meta_view_count_si);
+				
+		$doc->formatOutput = true;			
 	}
 		
 		$doc->save('extension/ez_aya_sitsearch/java/sit_data.xml');
