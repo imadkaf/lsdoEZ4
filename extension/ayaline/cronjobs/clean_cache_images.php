04bfb7864c0be1e596578103b32b7cf1e337e631
jmace <jmace@d7d630f7-2859-4ba2-aaf3-32abb50a90ba>
2012-02-08 15:34:44 +0000

    ajout d'un script pour nettoyer les fichiers images du cache (correction)

--- extension/ayaline/cronjobs/clean_cache_images.php
+++ extension/ayaline/cronjobs/clean_cache_images.php
@@ -10,8 +10,8 @@ $cli->output( $cli->stylize( 'black', 'Debut du script'), true );
 require_once( 'kernel/common/ezincludefunctions.php' );
 
 ob_start();
-exec(' cd var/ez_aya_sit/cache/images/', $output);
-exec('find -name "*" -mtime +7 -exec rm -rf {} \;', $output);
+//exec('cd var/ez_aya_sit/cache/images/', $output);
+exec('find var/ez_aya_sit/cache/images/ -name "*" -mtime +7 -exec rm -rf {} \;', $output);
 $content_grabbed=ob_get_contents();
 ob_end_clean();
 
