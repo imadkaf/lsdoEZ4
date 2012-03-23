66145d99866db7472cb4586cead622a90be82cab
jmace <jmace@d7d630f7-2859-4ba2-aaf3-32abb50a90ba>
2012-03-23 15:54:03 +0000

    Correction sur version anglaise

---
+++ trunk/extension/ayaline/translations/eng-GB/translation.ts
@@ -0,0 +1,525 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+	<context>
+	    <name>ayaline/langue</name>
+	    <message>
+	        <source>Langue</source>
+	        <translation>Language</translation>
+	    </message>
+	</context>
+	<context>
+		<name>sit/termes/override</name>
+		<message>
+			<source>DebutOuv</source>
+			<translation>Open at :</translation>
+		</message>
+		<message>
+			<source>FinOuv</source>
+			<translation>to :</translation>
+		</message>
+		<message>
+			<source>AdresseFiche</source>
+			<translation>Adresse</translation>
+		</message>
+		<message>
+			<source>Itineraire</source>
+			<translation>Route</translation>
+		</message>
+		<message>
+			<source>Categorie</source>
+			<translation>Category</translation>
+		</message>
+		<message>
+			<source>AffinerRecherche</source>
+			<translation>Validate the search</translation>
+		</message>
+		<message>
+			<source>Descriptif</source>
+			<translation>Description</translation>
+		</message>
+		<message>
+			<source>Caracteristiques</source>
+			<translation>Caracteristics</translation>
+		</message>
+		<message>
+			<source>EnsavoirplusReserver</source>
+			<translation>Learn more / Reserve</translation>
+		</message>
+		<message>
+			<source>AVoirAussi</source>
+			<translation>See too</translation>
+		</message>
+		<message>
+			<source>EnSavoirPlus</source>
+			<translation>Learn more</translation>
+		</message>
+		<message>
+			<source>ToutesLesFiches</source>
+			<translation>All the index</translation>
+		</message>
+		<message>
+			<source>TousLesEvenements</source>
+			<translation>All the events</translation>
+		</message>
+		<message>
+			<source>Agenda</source>
+			<translation>Agenda</translation>
+		</message>
+		<message>
+			<source>ToutesLesDates</source>
+			<translation>All the dates</translation>
+		</message>
+		<message>
+			<source>CoupDeCoeur</source>
+			<translation>Favorite</translation>
+		</message>
+		<message>
+			<source>LesPlusConsultes</source>
+			<translation>The most seen</translation>
+		</message>
+		<message>
+			<source>ToutLAgenda</source>
+			<translation>All the agenda</translation>
+		</message>
+		<message>
+			<source>AConsulter</source>
+			<translation>To consult</translation>
+		</message>
+		<message>
+			<source>DansLaMemeCategorie</source>
+			<translation>In the same category</translation>
+		</message>
+		<message>
+			<source>Reserver</source>
+			<translation>Reserve</translation>
+		</message>
+		<message>
+			<source>Dispos</source>
+			<translation>Availabilities</translation>
+		</message>
+		<message>
+			<source>AffinerVotreRecherche</source>
+			<translation>Ripen your search</translation>
+		</message>
+		<message>
+			<source>ReinitialiserRecherche</source>
+			<translation>Reset the search</translation>
+		</message>
+		<message>
+			<source>TrierPar</source>
+			<translation>Sort by</translation>
+		</message>
+		<message>
+			<source>TriCroissant</source>
+			<translation>Increasing sort</translation>
+		</message>
+		<message>
+			<source>TriDecroissant</source>
+			<translation>Decreasing sort</translation>
+		</message>
+		<message>
+			<source>AnnulerTri</source>
+			<translation>Cancel the sort</translation>
+		</message>
+		<message>
+			<source>Commune</source>
+			<translation>District</translation>
+		</message>
+		<message>
+			<source>MotsCles</source>
+			<translation>Key words</translation>
+		</message>
+		<message>
+			<source>APartirDe</source>
+			<translation>Start to </translation>
+		</message>
+		<message>
+			<source>NombreNuitees</source>
+			<translation>Nights number</translation>
+		</message>
+		<message>
+			<source>Indifferent</source>
+			<translation>—</translation>
+		</message>
+		<message>
+			<source>PremierePage</source>
+			<translation>First page</translation>
+		</message>
+		<message>
+			<source>DernierePage</source>
+			<translation>Last page</translation>
+		</message>
+		<message>
+			<source>Precedent</source>
+			<translation>Previous</translation>
+		</message>
+		<message>
+			<source>Suivant</source>
+			<translation>Next</translation>
+		</message>
+		<message>
+			<source>Aucun</source>
+			<translation>No card corresponds to your research</translation>
+		</message>
+		<message>
+			<source>ResultatsTrouves</source>
+			<translation>card(s) correspond(s) to your search</translation>
+		</message>
+		<message>
+			<source>OuvertTouteAnnee</source>
+			<translation>Open all the year</translation>
+		</message>
+		<message>
+			<source>PeriodesOuverture</source>
+			<translation>Openning periods</translation>
+		</message>
+		<message>
+			<source>Le</source>
+			<translation>The</translation>
+		</message>
+		<message>
+			<source>Du</source>
+			<translation>Of the</translation>
+		</message>
+		<message>
+			<source>Au</source>
+			<translation>to</translation>
+		</message>
+		<message>
+			<source>RetourListe</source>
+			<translation>Back to the list</translation>
+		</message>
+		<message>
+			<source>AdressePrestataire</source>
+			<translation>Provider address</translation>
+		</message>
+		<message>
+			<source>Telephone</source>
+			<translation>Phone</translation>
+		</message>
+		<message>
+			<source>Fax</source>
+			<translation>Fax</translation>
+		</message>
+		<message>
+			<source>InformationProposeePar</source>
+			<translation>This information is proposed to you by</translation>
+		</message>
+		<message>
+			<source>Janvier</source>
+			<translation>January</translation>
+		</message>
+		<message>
+			<source>Fevrier</source>
+			<translation>February</translation>
+		</message>
+		<message>
+			<source>Mars</source>
+			<translation>March</translation>
+		</message>
+		<message>
+			<source>Avril</source>
+			<translation>April</translation>
+		</message>
+		<message>
+			<source>Mai</source>
+			<translation>May</translation>
+		</message>
+		<message>
+			<source>Juin</source>
+			<translation>June</translation>
+		</message>
+		<message>
+			<source>Juillet</source>
+			<translation>July</translation>
+		</message>
+		<message>
+			<source>Aout</source>
+			<translation>August</translation>
+		</message>
+		<message>
+			<source>Septembre</source>
+			<translation>September</translation>
+		</message>
+		<message>
+			<source>Octobre</source>
+			<translation>October</translation>
+		</message>
+		<message>
+			<source>Novembre</source>
+			<translation>November</translation>
+		</message>
+		<message>
+			<source>Decembre</source>
+			<translation>December</translation>
+		</message>
+		<message>
+			<source>Ferme</source>
+			<translation>Close</translation>
+		</message>
+		<message>
+			<source>Complet</source>
+			<translation>Full</translation>
+		</message>
+		<message>
+			<source>Disponible</source>
+			<translation>Available</translation>
+		</message>
+		<message>
+			<source>ChainesDHotels</source>
+			<translation>Hotels chains</translation>
+		</message>
+		<message>
+			<source>Email</source>
+			<translation>Email</translation>
+		</message>
+		<message>
+			<source>SiteWeb</source>
+			<translation>Web site</translation>
+		</message>
+		<message>
+			<source>Avis</source>
+			<translation>Opinion</translation>
+		</message>
+		<message>
+			<source>AucunAvisPourLeMoment</source>
+			<translation>There is no opinion</translation>
+		</message>
+		<message>
+			<source>SoyezLePremier</source>
+			<translation>Be the first</translation>
+		</message>
+		<message>
+			<source>Tarifs</source>
+			<translation>Fees</translation>
+		</message>
+		<message>
+			<source>Hotels</source>
+			<translation>Hotels</translation>
+		</message>
+		<message>
+			<source>VoirAussi</source>
+			<translation>See too</translation>
+		</message>
+		<message>
+			<source>ToutesFichesMemeCategorie</source>
+			<translation>All the cards of the came category</translation>
+		</message>
+		<message>
+			<source>VotreAvis</source>
+			<translation>Your opinion</translation>
+		</message>
+		<message>
+			<source>ChangerDeSaison</source>
+			<translation>Change<![CDATA[<br />]]> season &gt;</translation>
+		</message>
+	</context>
+	
+	<context>
+	    <name>ayaline/pageSuivante</name>
+	    <message>
+	        <source>Page suivante</source>
+	        <translation>Next page</translation>
+	    </message>
+	</context>
+	<context>
+	    <name>ayaline/pagePrecedente</name>
+	    <message>
+	        <source>Page précédente</source>
+	        <translation>Previous page</translation>
+	    </message>
+	</context>
+	
+	<context>
+	    <name>ayaline</name>
+	    <message>
+	        <source>The informations are not available</source>
+	        <translation>The informations are not available</translation>
+	    </message>
+	    <message>
+	        <source>Learn more</source>
+	        <translation>Learn more</translation>
+	    </message>
+	    <message>
+	        <source>Download</source>
+	        <translation>Download</translation>
+	    </message>
+	    <message>
+	        <source>No photograph is available</source>
+	        <translation>No photograph is available</translation>
+	    </message>
+	    <message>
+	        <source>No video is available</source>
+	        <translation>No video is available</translation>
+	    </message>
+	    <message>
+	        <source>Close</source>
+	        <translation>Close</translation>
+	    </message>
+	    <message>
+	        <source>No booklet is available</source>
+	        <translation>No booklet is available</translation>
+	    </message>
+	    <message>
+	        <source>Hello</source>
+	        <translation>Hello</translation>
+	    </message>
+	    <message>
+	        <source>This email was sent automatically since the web site of tourist office of Sables d'Olonne</source>
+	        <translation>This email was sent automatically since the web site of tourist office of Sables d'Olonne</translation>
+	    </message>
+	    <message>
+	        <source>Your message was sent</source>
+	        <translation>Your message was sent</translation>
+	    </message>
+	    <message>
+	        <source>The team of the tourist office of Sables d'Olonne will bring to you an answer as soon as possible</source>
+	        <translation>The team of the tourist office of Sables d'Olonne will bring to you an answer as soon as possible</translation>
+	    </message>
+	    <message>
+	        <source>Home</source>
+	        <translation>Home</translation>
+	    </message>
+	    <message>
+	        <source>We thank you for your comprehension and let us invite you to continue your consultation of the site while clicking here</source>
+	        <translation>We thank you for your comprehension and let us invite you to continue your consultation of the site while clicking here</translation>
+	    </message>
+	    <message>
+	        <source>Back to the top</source>
+	        <translation>Back to the top</translation>
+	    </message>
+	    <message>
+	        <source>No question is available</source>
+	        <translation>No question is available</translation>
+	    </message>
+	    <message>
+	        <source>Please fill the following fields</source>
+	        <translation>Please fill the following fields</translation>
+	    </message>
+	    <message>
+	        <source>Your message</source>
+	        <translation>Your message</translation>
+	    </message>
+	    <message>
+	        <source>Your informations</source>
+	        <translation>Your informations</translation>
+	    </message>
+	    <message>
+	        <source>Your services</source>
+	        <translation>Your services</translation>
+	    </message>
+	    <message>
+	        <source>Security code</source>
+	        <translation>Security code</translation>
+	    </message>
+	    <message>
+	        <source>Tape the code below. If you cannot read all the letters or numbers</source>
+	        <translation>Tape the code below. If you cannot read all the letters or numbers</translation>
+	    </message>
+	    <message>
+	        <source>Clic here</source>
+	        <translation>Clic here</translation>
+	    </message>
+	    <message>
+	        <source>Reset</source>
+	        <translation>Reset</translation>
+	    </message>
+	    <message>
+	        <source>Send</source>
+	        <translation>Send</translation>
+	    </message>
+	    <message>
+	        <source>Required field</source>
+	        <translation>Required field</translation>
+	    </message>
+	    <message>
+	        <source>Others housings</source>
+	        <translation>Others housings</translation>
+	    </message>
+	    <message>
+	        <source>This page is not associated with the selected topic</source>
+	        <translation>This page is not associated with the selected topic</translation>
+	    </message>
+	    <message>
+	        <source>Linked documents</source>
+	        <translation>Linked documents</translation>
+	    </message>
+	    <message>
+	        <source>Search</source>
+	        <translation>Search</translation>
+	    </message>
+	    <message>
+	        <source>Relevance</source>
+	        <translation>Relevance</translation>
+	    </message>
+	    <message>
+	        <source>Last change</source>
+	        <translation>Last change</translation>
+	    </message>
+	    <message>
+	        <source>Connection</source>
+	        <translation>Connection</translation>
+	    </message>
+	    <message>
+	        <source>Page cannot be found</source>
+	        <translation>Page cannot be found</translation>
+	    </message>
+	    <message>
+	        <source>The page which you asked do not exist or is temporarily inalienable</source>
+	        <translation>The page which you asked do not exist or is temporarily inalienable</translation>
+	    </message>
+	    <message>
+	        <source>Sorry to cause embarrassments</source>
+	        <translation>Sorry to cause embarrassments</translation>
+	    </message>
+	    <message>
+	        <source>We propose to you</source>
+	        <translation>We propose to you</translation>
+	    </message>
+	    <message>
+	        <source>to return to home</source>
+	        <translation>to return to home</translation>
+	    </message>
+	    <message>
+	        <source>Access to press zone</source>
+	        <translation>Access to press zone</translation>
+	    </message>
+	    <message>
+	        <source>To access the zone press, it is necessary to you authenticate. For that, inform the fields below with the accesses which were provided to you</source>
+	        <translation>To access the zone press, it is necessary to you authenticate. For that, inform the fields below with the accesses which were provided to you</translation>
+	    </message>
+	    <message>
+	        <source>If you do not have an access you can have it by fill the contact form</source>
+	        <translation>If you do not have an access you can have it by fill the contact form</translation>
+	    </message>
+	    <message>
+	        <source>Logout</source>
+	        <translation>Logout</translation>
+	    </message>
+	    <message>
+	        <source>Your search</source>
+	        <translation>Your search</translation>
+	    </message>
+	    <message>
+	        <source>Automne</source>
+	        <translation>Autumn</translation>
+	    </message>
+	    <message>
+	        <source>Hiver</source>
+	        <translation>Winter</translation>
+	    </message>
+	    <message>
+	        <source>Printemps</source>
+	        <translation>Spring</translation>
+	    </message>
+	    <message>
+	        <source>Été</source>
+	        <translation>Summer</translation>
+	    </message>
+		<message>
+	        <source>Tide Tables</source>
+	        <translation>Tide Tables</translation>
+	    </message>
+	</context>
+</TS>
