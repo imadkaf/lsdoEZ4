97ced4f65f32a877a59ea65551cb7a74d6e1e735
ajarry <ajarry@d7d630f7-2859-4ba2-aaf3-32abb50a90ba>
2012-04-02 08:48:46 +0000

    Correction version jquery

--- trunk/extension/ayaline_mobi/settings/siteaccess/ayaline_mobi/design.ini.append.php
+++ trunk/extension/ayaline_mobi/settings/siteaccess/ayaline_mobi/design.ini.append.php
@@ -7,9 +7,7 @@ DesignExtensions[]=ayaline_mobi
 FrontendCSSFileList[]
 FrontendCSSFileList[]=stylesheet_std.css
 FrontendCSSFileList[]=jquery-ui-1.8.14.custom.css
-#FrontendCSSFileList[]=http://code.jquery.com/mobile/1.0a4.1/jquery.mobile-1.0a4.1.min.css
 FrontendCSSFileList[]=jquery.mobile-1.0a4.1.min.css
-#FrontendCSSFileList[]=http://code.jquery.com/mobile/1.0rc1/jquery.mobile-1.0rc1.min.css
 FrontendCSSFileList[]=jquery.ui.datepicker.mobile.css
 FrontendCSSFileListAndroid[]
 FrontendCSSFileListAndroid[]=stylesheet_android.css
@@ -23,17 +21,11 @@ FrontendCSSFileListPC[]=stylesheet_desktop.css
 
 [JavaScriptSettings]
 JavaScriptListMobile[]
-#La derniere version de la bibliotheque jquery dans la famille 1.x
-JavaScriptListMobile[]=http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js
-JavaScriptListMobile[]=http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js
+JavaScriptListMobile[]=jquery.min.js
+JavaScriptListMobile[]=jquery-ui.min.js
 JavaScriptListMobile[]=http://maps.google.com/maps/api/js?sensor=true&amp;key=ABQIAAAAPzlBh0JjFUPqZKi3n0L3LxRNhXKcoHc6ILCDtHOsJEw_kWBWgBR1kXeQdewQ9aUBq3FH6LbXaDfhmw
 #Complement de la bibliotheque jquery pour la version mobile
 JavaScriptListMobile[]=jquery.mobile-1.0a4.1.min.js
-#JavaScriptListMobile[]=http://code.jquery.com/mobile/1.0rc1/jquery.mobile-1.0rc1.min.js
-#A quoi sert ce script ?
-#JavaScriptListMobile[]=http://code.google.com/apis/gears/gears_init.js
-#Utilise directement dans la fiche
-#JavaScriptListMobile[]=http://ajax.googleapis.com/ajax/libs/jqueryui/1/jquery-ui.min.js
 JavaScriptListMobile[]=itineraire.js
 JavaScriptListMobile[]=listecarte.js
 JavaScriptListMobile[]=jQuery.ui.datepicker.js
