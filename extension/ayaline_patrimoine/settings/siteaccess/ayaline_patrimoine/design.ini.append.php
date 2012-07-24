51ff606638c161ea829fbf913923dd50c9afb971
melbajjati <melbajjati@d7d630f7-2859-4ba2-aaf3-32abb50a90ba>
2012-07-24 12:49:31 +0000

    [LSDO Patrimoine] - Intégration des modifications de la maquette statique.

--- extension/ayaline_patrimoine/settings/siteaccess/ayaline_patrimoine/design.ini.append.php
+++ extension/ayaline_patrimoine/settings/siteaccess/ayaline_patrimoine/design.ini.append.php
@@ -5,6 +5,8 @@ DesignExtensions[]=ayaline_patrimoine
 
 [StylesheetSettings]
 CSSFileListPatrimoine[]
+CSSFileListPatrimoine[]=styles.css
+CSSFileListPatrimoine[]=photoswipe.css
 CSSFileListPatrimoine[]=main.css
 CSSFileListPatrimoine[]=jplayer.blue.monday.css
 CSSFileListPatrimoine[]=jquery.mobile.css
@@ -18,5 +20,6 @@ JavaScriptListPatrimoine[]=jquery.mobile.min.js
 JavaScriptListPatrimoine[]=script.js
 JavaScriptListPatrimoine[]=head.min.js
 JavaScriptListPatrimoine[]=jquery.jplayer.min.js
+JavaScriptListPatrimoine[]=code.photoswipe-3.0.5.min.js
 
 */ ?>
