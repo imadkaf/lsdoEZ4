dd712ff5854266d19210c624dc75a79e070fa0e8
jmace <jmace@d7d630f7-2859-4ba2-aaf3-32abb50a90ba>
2012-08-16 09:35:44 +0000

    mantis 339 : téléchargement des brochures en GB

---
+++ trunk/extension/faq/settings/siteaccess/ayaline_en/override.ini.append.php
@@ -0,0 +1,21 @@
+<?php /* #?ini charset="utf-8"?
+
+[full_faqs]
+Source=node/view/full.tpl
+MatchFile=full/faq_list_view_full.tpl
+Subdir=templates
+Match[class_identifier]=faqs
+
+[line_faq]
+Source=node/view/line.tpl
+MatchFile=line/faq_view_list.tpl
+Subdir=templates
+Match[class_identifier]=faq
+
+[grouped_faq]
+Source=node/view/grouped.tpl
+MatchFile=line/faq_view_grouped.tpl
+Subdir=templates
+Match[class_identifier]=faq
+
+*/ ?>
