3a5b8861fde1116f06d51ca95c562a60a28eb23a
cesculier <cesculier@d7d630f7-2859-4ba2-aaf3-32abb50a90ba>
2012-09-04 08:05:19 +0000

    [Patrimoine] Icones

--- trunk/extension/ayaline_patrimoine/settings/icon.ini.append.php
+++ trunk/extension/ayaline_patrimoine/settings/icon.ini.append.php
@@ -19,6 +19,7 @@ ClassMap[smp_audio]=lsdo_patrimoine/smp_audio.png
 ClassMap[smp_diaporama]=lsdo_patrimoine/smp_diaporama.png
 ClassMap[smp_image]=lsdo_patrimoine/smp_image.png
 ClassMap[smp_mise_avant_brute]=lsdo_patrimoine/calendar-empty.png
-ClassMap[smp_quizz]=lsdo_patrimoine/smp_quiz.png
+ClassMap[smp_quizz]=lsdo_patrimoine/smp_quizz.png
+ClassMap[smp_declinaison]=lsdo_patrimoine/smp_declinaison.png
 
 */ ?>
