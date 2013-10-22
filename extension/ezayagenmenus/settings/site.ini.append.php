fac6152adf118b0c40c86cf9eb264783ccce9fb6
melbajjati <melbajjati@d7d630f7-2859-4ba2-aaf3-32abb50a90ba>
2013-10-22 14:35:27 +0000

    Footer & Header UK : Ajout des configurations pour générer les nouvelles urls, ex: http://genmenu.lsdo.uk/genmenu/header http://genmenu.lsdo.uk/genmenu/footer
    Pour plus d'infos voir (doc/Install).

--- extension/ezayagenmenus/settings/site.ini.append.php
+++ extension/ezayagenmenus/settings/site.ini.append.php
@@ -2,8 +2,11 @@
 
 [SiteAccessSettings]
 AvailableSiteAccessList[]=ezayagenmenus
+AvailableSiteAccessList[]=uk_ezayagenmenus
 RelatedSiteAccessList[]=ezayagenmenus
+RelatedSiteAccessList[]=uk_ezayagenmenus
 HostMatchMapItems[]=genmenu.lsdo.loc;ezayagenmenus
+HostMatchMapItems[]=genmenu.lsdo.uk;uk_ezayagenmenus
 
 [TemplateSettings]
 ExtensionAutoloadPath[]=ezayagenmenus
